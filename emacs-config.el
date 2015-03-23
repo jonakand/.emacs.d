e67a1e45a63f6f4d0df61c0274c5ab2314ca2fa3
Jonakand <jonakand@gmail.com>
2015-03-23 14:54:05 -0400

    Add FCI mode.

--- emacs-config.el
+++ emacs-config.el
@@ -1512,3 +1512,29 @@ _X_ Xquery region
   (progn
     (global-set-key (kbd "M-i") 'change-inner)
     (global-set-key (kbd "M-o") 'change-outer)))
+
+(use-package fill-column-indicator
+  :ensure t
+  :config
+  (progn    
+    (defun sanityinc/fci-enabled-p () (symbol-value 'fci-mode))
+    
+    (defvar sanityinc/fci-mode-suppressed nil)
+    (make-variable-buffer-local 'sanityinc/fci-mode-suppressed)
+    
+    (defadvice popup-create (before suppress-fci-mode activate)
+      "Suspend fci-mode while popups are visible"
+      (let ((fci-enabled (sanityinc/fci-enabled-p)))
+        (when fci-enabled
+          (setq sanityinc/fci-mode-suppressed fci-enabled)
+          (turn-off-fci-mode))))
+    
+    (defadvice popup-delete (after restore-fci-mode activate)
+      "Restore fci-mode when all popups have closed"
+      (when (and sanityinc/fci-mode-suppressed
+                 (null popup-instances))
+        (setq sanityinc/fci-mode-suppressed nil)
+        (turn-on-fci-mode)))
+
+    (setq fci-rule-column 80)
+    (fci-mode 1)))
