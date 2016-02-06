b7e7e0bf27488045d3ef524cd83fc14b2fe0d284
jonakand <jonakand@gmail.com>
2016-02-06 13:43:29 -0500

    Update init.
    Move loading S into init so that it is available in the main config.
    Update check to determine if it is a work machine or not.  Only
    load eclim if at work.

--- init.el
+++ init.el
@@ -1,27 +1,30 @@
 ;(let ((file-name-handler-alist nil))
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;;  Bootstrap use-package.
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (require 'package)
-  (setq package-enable-at-startup nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Bootstrap use-package.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+(setq package-enable-at-startup nil)
 
-  (setq package-archives '(("org" . "http://orgmode.org/elpa/") 
-                           ("gnu" . "http://elpa.gnu.org/packages/")
-                           ("melpa" . "http://melpa.org/packages/")))
+(setq package-archives '(("org" . "http://orgmode.org/elpa/") 
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
 
-  (package-initialize)
+(package-initialize)
 
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+;;  Load use-package and s early as they are needed in the main config.
+(unless (and (package-installed-p 'use-package)
+             (package-installed-p 's))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-  (eval-when-compile
-    (require 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
-  (require 'diminish)
-  (require 'bind-key)
+(require 'diminish)
+(require 'bind-key)
+(require 's)
 
-  (setq use-package-verbose t)   
+(setq use-package-verbose t)   
 
 (add-hook 'after-init-hook (lambda () (org-babel-load-file "~/.emacs.d/emacs-config.org")))
 
